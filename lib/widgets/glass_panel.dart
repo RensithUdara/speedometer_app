16bc05ad65f76a15ae8d6ea3c1ea9b6a18a425c8
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-09-14 18:59:43 +0530

    feat: add GlassPanel widget for customizable UI container

---
+++ lib/widgets/glass_panel.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class GlassPanel extends StatelessWidget {
+  const GlassPanel({
+    super.key,
+    required this.child,
+    this.padding = const EdgeInsets.all(14),
+  });
+
+  final Widget child;
+  final EdgeInsets padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: const Color(0xFF071B31).withValues(alpha: 0.72),
+        borderRadius: BorderRadius.circular(18),
+        border:
+            Border.all(color: const Color(0xFF245E9C).withValues(alpha: 0.72)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.22),
+            blurRadius: 18,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
